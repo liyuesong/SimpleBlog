7f2cab980b273a193eaa9ab133e2356212c801b4
liyuesong <liyuesong1943@gmail.com>
2018-01-15 21:32:52 -0800

    redux form - submission and navigation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 	renderField(field) {
@@ -8,22 +11,44 @@ class PostsNew extends Component {
 		// it is responsible for this input
 		// field.input is an object that contains some event handlers (onChange, onFocus, onBlur...) and some props
 		// ...: makes all props of field.input to be communicated to be as props to the <input> tag
+		const { meta: { touched, error } } = field;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input
 					className="form-control"
 					type="text"
 					{...field.input}
 				/>
-				{field.meta.error}
+				<div className="text-help">
+					{touched ? error : ''}
+				</div>
 			</div>
 		);
 	}
 
+	onSubmit(values) {
+		// this === component: bind
+		// an object contains all information input
+		// send it to back end (post API)
+		
+		// attempt to do the navigation AFTER the request
+		// this.props.history.push('/');
+		
+		// calling an action creactor which is responsible for posting the data to API
+		this.props.createPost(values, () => {
+			this.props.history.push('/');
+		});
+	}
+
 	render() {
+		// a property that been passed to the component on behalf of Redux Form
+		const { handleSubmit } = this.props;
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<Field 
 					label="Title For Post"
 					name="title"
@@ -39,6 +64,11 @@ class PostsNew extends Component {
 					name="content"
 					component={this.renderField}
 				/>
+
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+				<Link to="/" className="btn btn-danger">Cancel</Link>
 			</form>
 		);
 	}
@@ -56,7 +86,7 @@ function validate(values) {
 	// 	errors.title = "The title must be at least 3 characters";
 	// }
 	if (!values.title) {
-		errors.title = "Enter a title please!";
+		errors.title = "Enter a title please";
 	}
 	if (!values.categories) {
 		errors.categories = "Enter some categories";
@@ -70,8 +100,11 @@ function validate(values) {
 	return errors;
 }
 
+// add additional properties to the component PostsNew
 export default reduxForm({
 	validate,
 	// validate: validate,
 	form: 'PostsNewForm'
-})(PostsNew);
+})(
+	connect(null, { createPost })(PostsNew)
+);
